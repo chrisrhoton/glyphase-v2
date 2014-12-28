dc8173bef93b00d6d92cb3237ae3be0d59ffb095
Chris Rhoton <cjrhoton02@gmail.com>
2014-12-28 15:24:16 -0800

    Create user model with secure password along with signup form including error messages on invalid submission.

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save { self.email = email.downcase }
+  before_save { self.username = username.downcase}
+
+  validates :username,  presence: true, length: { maximum: 50 },
+                        uniqueness: { case_sensitive: false }
+
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
+
+  resources :users
+
+  match '/signup', to: 'users#new', via: 'get'
   
 end
