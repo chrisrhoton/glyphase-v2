87f5500b2c9b004638aa53bfc91e67e76d31b448
Chris Rhoton <cjrhoton02@gmail.com>
2014-12-30 22:11:15 -0800

    Add ability to update user profiles.  Create admin class of users.  Allow admins to display list of users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227065856) do
+ActiveRecord::Schema.define(version: 20141231051003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20141227065856) do
     t.string   "password_digest"
     t.string   "profile_image"
     t.string   "remember_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(username:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+User.create!(username:  "chris",
+             email: "chris.rhoton@gmail.com",
+             password:              "bewd2014",
+             password_confirmation: "bewd2014",
+             admin: true)
+
+99.times do |n|
+  username  = Faker::Internet.user_name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(username:              username,
+               email:                 email,
+               password:              password,
+               password_confirmation: password)
+end
