bd213fbfdf1f777217590b5c217a347ab578347f
Chris Rhoton <cjrhoton02@gmail.com>
2015-02-01 22:49:43 -0800

    Add password reset and configure SendGrid on Heroku.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,17 +4,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "Glyphbase - Account activation"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Glyphbase - Password reset"
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,11 +6,22 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:chris)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
+    assert_equal "Glyphbase - Account activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@glyphbase.com"], mail.from
     assert_match user.username,           mail.body.encoded
     assert_match user.activation_token,   mail.body.encoded
+    assert_match CGI::escape(user.email), mail.body.encoded
+  end
+
+  test "password_reset" do
+    user = users(:chris)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Glyphbase - Password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@glyphbase.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
   end
 
