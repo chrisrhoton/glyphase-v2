315fb9bbac123870d00d1e44dd404790d28158a3
Chris Rhoton <cjrhoton02@gmail.com>
2015-02-01 22:01:17 -0800

    Add user activations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141231051003) do
+ActiveRecord::Schema.define(version: 20150202050948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,12 @@ ActiveRecord::Schema.define(version: 20141231051003) do
     t.string   "password_digest"
     t.string   "profile_image"
     t.string   "remember_digest"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.boolean  "admin",           default: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,17 @@ User.create!(username:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
 
 User.create!(username:  "chris",
              email: "chris.rhoton@gmail.com",
              password:              "bewd2014",
              password_confirmation: "bewd2014",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   username  = Faker::Internet.user_name
@@ -17,5 +21,7 @@ User.create!(username:  "chris",
   User.create!(username:              username,
                email:                 email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
