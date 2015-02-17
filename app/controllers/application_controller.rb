1802ae2a0910cc2748fa8c538b080e8c68273cff
Chris Rhoton <cjrhoton02@gmail.com>
2015-02-17 14:00:54 -0800

    Add XSRF-TOKEN cookie for use with async requests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   include GlyphsHelper
+
+  after_filter :set_csrf_cookie_for_async
+
+  def set_csrf_cookie_for_async
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  protected
+
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
   
   private
 
